0084ba8848de70773e5f96048f3ba062e40b4c1a
Philipp Otto <philotto95@gmail.com>
2022-01-22 20:27:55 +0100

    Write createMultipleBlocks function

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,16 @@
-function createBlock(sideLength) {
+function createBlock(sideLengthBlock) {
     const container = document.querySelector('.container');
     const block = document.createElement('div');
-    block.style.cssText = `border: 2px solid blue; width: ${sideLength}px; height: ${sideLength}px;`;
+    block.style.cssText = `border: 2px solid blue; width: ${sideLengthBlock}px; height: ${sideLengthBlock}px;`;
     container.appendChild(block);
 }
 
-createBlock(20);
+function createMultipleBlocks(numberOfBlocksPerRow) {
+    const sideLengthContainer = document.querySelector('.container').clientWidth;
+    const sideLengthBlock = sideLengthContainer/numberOfBlocksPerRow;
+    const numberOfNewBlocks = numberOfBlocksPerRow**2;
+    for (let i = 0; i<numberOfNewBlocks; i++) {
+        createBlock(sideLengthBlock);
+    }
+}
+createMultipleBlocks(2);
